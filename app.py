48996c1b4873875d73b0b4bc8997ed5c94e7ce6c
Abdaisy conceicao <abdaisyconceicao@epfundao.edu.pt>
2024-10-15 13:33:44 +0100

    app.py modificação

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-# Inicializando o carrinho
-carrinho = []
-
-@app.route('/')
-def index():
-    return render_template('index2.html')  # Ou 'index.html' se você quiser usar esse
-
-@app.route('/<form_id>', methods=['GET'])
-def mostrar_formulario(form_id):
-    global carrinho
-    
-    forms = ['formVenderProduto', 'formAdicionarProduto', 'formRemoverProduto']
-    if form_id in forms:
-        carrinho = []  # Limpa o carrinho se o formulário correspondente for mostrado
-        return jsonify({'message': f'Formulário {form_id} mostrado com sucesso.'}), 200
-    else:
-        return jsonify({'error': 'Formulário não encontrado.'}), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
 
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
