6fe5f40e43587b7f77f7b974e4eb06f908de86c5
lara617 <laramp617@gmail.com>
2024-10-18 14:39:54 +0100

    firebase

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
 app = Flask(__name__)
+
+# Acessa a variável de ambiente
+cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 @app.route('/')
 def home():
@@ -15,4 +23,5 @@ def index3():
     return render_template('index3.html')
 
 if __name__ == '__main__':
+    print(f"dreambuilder-5a4ea-firebase-adminsdk-ek7ia-1d448da923.json")  # Apenas para verificar se a variável foi carregada corretamente
     app.run(debug=True)
